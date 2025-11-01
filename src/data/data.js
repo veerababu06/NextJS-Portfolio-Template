d8c23c2ffb97158a52642615f8f5356d1e34c968
veerababu <veerababu.uppala06@gmail.com>
2025-11-01 19:05:44 +0530

    added email

--- src/data/data.js
+++ src/data/data.js
@@ -2,7 +2,7 @@ export const about = {
     name: "Veerababu Uppala",
     role: "AWS DevOps Engineer",
     about: "A minimal, clean portfolio template built with Next.js. Perfect for developers who want a simple yet professional online presence without the complexity.",
-    email: "your.email@example.com",
+    email: "veerababu.uppala06@gmail.com",
 };
 
 export const seo = {
