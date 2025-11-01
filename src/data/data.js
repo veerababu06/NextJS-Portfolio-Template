ae5208469dd507f1f1e744b4cb57dba08f61341f
veerababu <veerababu.uppala06@gmail.com>
2025-11-01 19:03:38 +0530

    added profission

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
 export const about = {
     name: "Veerababu Uppala",
-    role: "Your Profession",
+    role: "AWS DevOps Engineer",
     about: "A minimal, clean portfolio template built with Next.js. Perfect for developers who want a simple yet professional online presence without the complexity.",
     email: "your.email@example.com",
 };
